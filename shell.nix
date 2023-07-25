e3908470288e41c31445e47e1f84c39b0b12bf39
Charlotte <charlotte@hrzn.ee>
2023-07-25 10:44:03 +1000

    shell.nix: alejandra.

--- shell.nix
+++ shell.nix
@@ -1,15 +1,15 @@
-with import <nixpkgs> {};
-let
+with import <nixpkgs> {}; let
   env = bundlerEnv {
     name = "notes-bundler-env";
     inherit ruby;
-    gemfile  = ./Gemfile;
+    gemfile = ./Gemfile;
     lockfile = ./Gemfile.lock;
     # NOTE: I've hacked in the platforms for commonmarker and nokogiri.
     # https://github.com/nix-community/bundix/issues/71
-    gemset   = ./gemset.nix;
+    gemset = ./gemset.nix;
   };
-in stdenv.mkDerivation {
-  name = "notes";
-  buildInputs = [ env ];
-}
+in
+  stdenv.mkDerivation {
+    name = "notes";
+    buildInputs = [env];
+  }
